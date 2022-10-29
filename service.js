4010b597e2e8a17db9201de2f033104e6b5da17e
boldak <boldak.andrey@gmail.com>
2022-10-29 21:18:26 +0300

    prepare for deploy

--- service.js
+++ service.js
@@ -1,17 +1,17 @@
-const { ServiceWrapper } = require("@molfar/csc")
-const { AmqpManager, Middlewares } = require("@molfar/amqp-client")
+const { ServiceWrapper, AmqpManager, Middlewares } = require("@molfar/service-chassis")
 const { extend } = require("lodash")
 const path = require("path")
 
 
 const NER = require("./src/javascript/bridge")
- const config = {
+ 
+const config = {
      mode: 'text',
      encoding: 'utf8',
      pythonOptions: ['-u'],
      pythonPath: (process.env.NODE_ENV && process.env.NODE_ENV == "production") ? 'python' : 'python.exe',
      args: path.resolve(__dirname,"./MITIE-models/model.dat")	
- }
+}
 
  const extractor = new NER(config)
 
@@ -37,24 +37,31 @@ let service = new ServiceWrapper({
             Middlewares.Error.Log,
             Middlewares.Error.BreakChain,
             
+
+            async (err, msg, next) => {
+                console.log("CONSUME", msg.content)
+                next()
+            },    
+
             Middlewares.Filter( msg =>  {
-                if( msg.content.metadata.nlp.language.locale != "uk") {
-                    console.log(`${ this.config._instance_name || this.config._instance_id} ignore `, msg.content.md5, msg.content.metadata.nlp.language.locale)
+                if( msg.content.langDetector.language.locale != "uk") {
+                    console.log("IGNORE", msg.content.langDetector.language.locale)
                     msg.ack()
+                } else {
+                    console.log("ACCEPT", msg.content.langDetector.language.locale)
                 } 
-                return msg.content.metadata.nlp.language.locale == "uk"
+                return msg.content.langDetector.language.locale == "uk"
             }),
 
             async (err, msg, next) => {
                 let m = msg.content
-                let res = await extractor.getNER(m.metadata.text)
-                m.metadata.nlp = extend({}, m.metadata.nlp, 
-                    {
+                let res = await extractor.getNER(m.scraper.message.text)
+                m = extend({}, m, {
                         ner: res.data.response.named_entities
                     }
                 )
                 this.publisher.send(m)
-                console.log(`${ this.config._instance_name || this.config._instance_id} recognize `, m.md5, `${res.data.response.named_entities.length} entities`)
+                console.log(`RECOGNIZE ${res.data.response.named_entities.length} named entities`)
                 msg.ack()
             }
 
@@ -69,20 +76,16 @@ let service = new ServiceWrapper({
             Middlewares.Json.stringify
         ])
 
-
-
         resolve({ status: "configured" })
 
     },
 
     onStart(data, resolve) {
-        console.log(`start ${ this.config._instance_name || this.config._instance_id}`)
         this.consumer.start()
         resolve({ status: "started" })
     },
 
     async onStop(data, resolve) {
-        console.log(`stop ${ this.config._instance_name || this.config._instance_id}`)
         await this.consumer.close()
         await this.publisher.close()
         resolve({ status: "stoped" })
